795886d309151b4d86064d4cf49c4177b8f067cb
lewtech <lewtech@gmail.com>
2016-01-27 10:37:04 -0600

    Initial Commit

---
+++ AllstateCar AudioMeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AllstateCar AudioMeter
+//
+//  Created by Lew Flauta on 1/27/16.
+//  Copyright (c) 2016 Lew Flauta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
